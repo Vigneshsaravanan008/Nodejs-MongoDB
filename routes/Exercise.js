89c3365528f27ae9066e46641cc4accfa29c2e0f
VigneshSaravanan <76770922+VigneshSaravanan26@users.noreply.github.com>
2022-07-10 17:47:51 +0530

    Mongodb Curd Added"

--- routes/Exercise.js
+++ routes/Exercise.js
@@ -24,3 +24,30 @@ router.route('/add').get((req,res)=>{
     .then(()=>res.json('Exercise Added'))
     .catch(err=>res.status(400).json('Error'+err));
 })
+
+router.route('/:id').get((req,res)=>{
+    Exercise.findById(req,body.id)
+    .then(exercise=>res.json(exercise))
+    .catch(err=>res.status(400).json('error'+err));
+})
+
+router.route('/:id').delete((req,res)=>{
+    Exercise.findByIdAndDelete(req,body.id)
+    .then(()=>res.json('Exercise deleted'))
+    .catch(err=>res.status(400).json('error'+err));
+})
+
+router.route('/update/:id').get((req,res)=>{
+    Exercise.findById(req,body.id)
+    .then(exercise=>{
+        exercise.username=req.body.username;
+        exercise.description=req.body.description;
+        exercise.duration=Number(req.body.duration);
+        exercise.date=Date(req.body.date);
+
+        exercise.save()
+        .then(()=>res.json('Exercise Updated'))
+        .catch(err=>res.status(400).json('Error'+err));
+    })
+    .catch(err=>res.status(400).json('error'+err));
+})
